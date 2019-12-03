b322352a2094b06f5b66ae5c26ae0a0861446308
Abdullah <aalthawr@gmail.com>
2019-12-04 04:05:25 +0800

    Updates

---
+++ precache-manifest.e3fc4aac7aa5a9c391736454b8862673.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67369a9faddd420b7dfce5451e6c91d7",
+    "url": "/mazada/index.html"
+  },
+  {
+    "revision": "7759fe680ec79dfd7c97",
+    "url": "/mazada/static/css/2.e6157a56.chunk.css"
+  },
+  {
+    "revision": "85d7cca876a3a906d02e",
+    "url": "/mazada/static/css/main.8d1bcb37.chunk.css"
+  },
+  {
+    "revision": "7759fe680ec79dfd7c97",
+    "url": "/mazada/static/js/2.3541fad0.chunk.js"
+  },
+  {
+    "revision": "85d7cca876a3a906d02e",
+    "url": "/mazada/static/js/main.ee0b2a03.chunk.js"
+  },
+  {
+    "revision": "36291af46e61f3247eed",
+    "url": "/mazada/static/js/runtime-main.2f9bd110.js"
+  },
+  {
+    "revision": "68f1f977d6a18324c95fb4a19783d73c",
+    "url": "/mazada/static/media/cover.68f1f977.png"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/mazada/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "2ee1745b009a66477a3eab3601e4c62e",
+    "url": "/mazada/static/media/mazada.2ee1745b.png"
+  }
+]);
