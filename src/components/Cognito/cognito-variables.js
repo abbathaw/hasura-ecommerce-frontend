09472a775ce916439d131167f03b097afd990cb4
Abdullah <aalthawr@gmail.com>
2019-12-05 11:03:12 +0800

    Minor code adjustments

--- src/components/Cognito/cognito-variables.js
+++ src/components/Cognito/cognito-variables.js
@@ -1,14 +1,14 @@
 export const AUTH_CONFIG = {
-  domain: process.env.REACT_APP_DOMAIN || 'mazada.auth.us-west-2.amazoncognito.com',
-  clientId: process.env.REACT_APP_CLIENT_ID || '3kl7hui7a7p7f9tc5qvpn9pupj',
+  domain: process.env.REACT_APP_DOMAIN,
+  clientId: process.env.REACT_APP_CLIENT_ID,
   responseType: 'token',
-  redirectUri: process.env.REACT_APP_BASE_URL  || 'http://localhost:3000',
-  signoutUri: process.env.REACT_APP_BASE_URL|| 'http://localhost:3000',
+  redirectUri: process.env.REACT_APP_BASE_URL,
+  signoutUri: process.env.REACT_APP_BASE_URL,
   tokenScopes: [
     "openid",
     "email",
     "profile"
   ],
-  UserPoolId: process.env.REACT_APP_USER_POOL_ID || "us-west-2_9np3c0hBN",
-  region: process.env.REACT_APP_REGION || "us-west-2",
+  UserPoolId: process.env.REACT_APP_USER_POOL_ID,
+  region: process.env.REACT_APP_REGION,
 };
